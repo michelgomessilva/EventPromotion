ae81e113e928c4d714d44769158538709f6af024
Michel Silva <michel.gomes.silva@gmail.com>
2024-06-26 22:38:43 +0100

    First Commit

---
+++ back-end/Studies.Michel.PromoEvents.API/Controllers/EventController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Studies.Michel.PromoEvents.API.Models;
+
+namespace Studies.Michel.PromoEvents.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        public EventController()
+        {            
+        }
+
+        [HttpGet]
+        public IEnumerable<EventModel> Get()
+        {
+            return new EventModel[]
+            {
+                new EventModel()
+                {
+                    EventId = 1,
+                    Theme = "Angular 11 e .Net Core 8",
+                    Location = "Porto",
+                    Batch = "1º Lote",
+                    PeopleCount = 250,
+                    EventDate = DateTime.Now.AddDays(2).ToString(),
+                    ImageURL = "foto.jpg"
+                }
+            };
+        }
+    }
+}
---
+++ back-end/Studies.Michel.PromoEvents.API/Models/EventModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Studies.Michel.PromoEvents.API.Models
+{
+    public class EventModel
+    {
+        public int EventId { get; set; }
+        public string Location { get; set; }
+        public string EventDate { get; set; }
+        public string Theme { get; set; }
+        public int PeopleCount { get; set; }
+        public string Batch { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
